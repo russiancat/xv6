e8587d664b5b6014871ee3906e1bd04679253988
Ubuntu <vipul.sawant@gmail.com>
2023-02-14 04:41:14 +0530

    add priority in procdump

--- proc.c
+++ proc.c
@@ -552,7 +552,7 @@ procdump(void)
       state = states[p->state];
     else
       state = "???";
-    cprintf("%d %s %s", p->pid, state, p->name);
+    cprintf("%d %d %s %s", p->pid, p->priority, state, p->name);
     if(p->state == SLEEPING){
       getcallerpcs((uint*)p->context->ebp+2, pc);
       for(i=0; i<10 && pc[i] != 0; i++)
